2d7d042724a1246ea75481a681a298feb8bf5e2f
Lassi Köykkä <lassi_koykka@hotmail.com>
2021-12-22 20:46:31 +0200

    Day 12 done

--- day12/main.py
+++ day12/main.py
@@ -1,33 +1,94 @@
 import time
 
-test = 1
+test = 0
 
 # Setup here
 f = open("inputtest" if test else "input")
 fileContent = f.read()
-data = fileContent.strip().split(",")
+lines = fileContent.strip().split("\n")
+data = [ l.split("-") for l in lines]
 
+neighbours: dict[str,list[str]] = {}
+for path in data:
+    v1 = path[0]
+    v2 = path[1]
+    if(neighbours.get(v1, None) == None):
+        neighbours[v1] = []
+    if(neighbours.get(v2, None) == None):
+        neighbours[v2] = []
+    neighbours[v1].append(v2)
+    neighbours[v2].append(v1)
+            
 def puzzle1 (input):
-    startTime = time.time()
+    paths = 0
 
+    def searchPaths(visited, v):
+        newPaths = 0
+        currVisited = visited.copy()
+        currVisited.append(v)
+        for n in input[v]:
+            if(n == "start"): continue
+            elif(n == "end"): 
+                newPaths += 1
+                currVisited.append(n)
+                #print(",".join(currVisited))
+            elif(n == n.upper() or visited.count(n) < 1):
+                newPaths += searchPaths(currVisited, n)
+
+        return newPaths
+    # ------------------
+
+    startTime = time.time()
+    
     # CODE
+    for n in input["start"]:
+        paths += searchPaths(["start"], n)
 
     endTime = time.time()
 
-    print()
+    print(paths)
     print("Took", (endTime - startTime) * 1000000, "us")
 
+# Very slow. Could be way better.
 def puzzle2 (input):
-    startTime = time.time()
+    paths = 0
 
+    def searchPaths(visited, v, smallVisitedTwice):
+        newPaths = 0
+        currVisited = visited.copy()
+        currVisited.append(v)
+
+        svt = bool(smallVisitedTwice)
+        if(svt == False):
+            for v in currVisited:
+                if(v == v.lower() and currVisited.count(v) > 1):
+                    svt = True
+
+        if(v == "end"):
+                newPaths += 1
+                #print(",".join(currVisited))
+                return newPaths
+
+        for n in input[v]:
+            if(n == "start"): continue
+            elif(n == n.upper() or (n not in currVisited) or (currVisited.count(n) < 2 and not svt)):
+                newPaths += searchPaths(currVisited, n, svt)
+
+        return newPaths
+    # ------------------
+
+    startTime = time.time()
+    
     # CODE
+    for n in input["start"]:
+        paths += searchPaths(["start"], n, False)
 
     endTime = time.time()
 
-    print()
+    print(paths)
     print("Took", (endTime - startTime) * 1000000, "us")
     
 
 # RUN 
-puzzle1(data)
-#puzzle2(data)
+#puzzle1(neighbours)
+puzzle2(neighbours)
