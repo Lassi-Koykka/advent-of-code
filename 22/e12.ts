b70a39451a1c228e8ed2ee69190164dd6d13eeb3
Lassi Köykkä <lassi_koykka@hotmail.com>
2022-12-14 21:40:22 +0200

    2022 day 12 done

--- 22/e12.ts
+++ 22/e12.ts
@@ -3,85 +3,185 @@ import { getContent } from "./utils"
 
 const CREATE_FILE = false
 const DAY = __filename.split(".")[0]
-const TEST = true
+const TEST = false
 
 const input = getContent(TEST ? DAY + "-test.txt" : DAY + "-input.txt", true)
 
 const CHAR_START = 97
 
-const printGrid = (grid: any[][]) => {
-  log()
-  log(grid.map(r => r.map(c => c === Infinity ? "." : "" + c).join(",")).join("\n"))
-  log()
+const getAdjecent = (grid: any[][], x: number, y: number, ) => {
+    const h = grid.length
+    const w = grid[0].length
+
+    return [
+      [x+1, y  ],
+      [x-1, y  ],
+      [x,   y-1],
+      [x,   y+1],
+    ].filter(([ax, ay]) => 
+      !(ax < 0 || ay < 0 || ax > w - 1 || ay > h - 1)
+    )
 }
 
-const findPos = (num: number, grid: number[][]) => 
-  grid.reduce((prev, r, y) => {
-    const x = r.findIndex(n => n === num) ?? -1
-    return x !== -1 ? [y, x] : prev
-  }, [-1,-1])
-
+// RIP Functional approach
 const dijkstra = 
-  ({grid, shortestPaths, cx, cy}: 
-   {grid: number[][], shortestPaths: number[][], cx: number, cy: number}) => {
-    const adj = [
-      [cx+1, cy  ],
-      [cx-1, cy  ],
-      [cx,   cy-1],
-      [cx,   cy+1],
-    ].filter(([ax, ay]) => ax >= 0 && ay >= 0)
+  ({nodes, cur, end}: 
+   {
+     nodes: {[key: string]: Node}, 
+     cur: string
+     end: string
+   }) => {
+     const curNode = nodes[cur]
+     if(cur === end) 
+       return nodes;
 
-    log("cur:", cx, cy, "->", adj.map(([ax,ay]) => ax + "," + ay + " " + grid[ay][ax]))
+     log(cur)
 
-  return adj.reduce((prevShortest, [x, y]) => {
-    try {
-      log("Processing", x,y)
-      if((((grid[y][x] - grid[cy][cx]) > 1 && grid[y][x] !== -1) 
-          || prevShortest[y][x] <= prevShortest[cy][cx] + 1))
-        return prevShortest
+     const adjUpdatedNodes = curNode.adj
+       .reduce((prev, id, i) => {
+         const a = prev[id]
+         if(a.visited || a.cost < curNode.cost + 1) 
+           return prev;
+         return {
+           ...prev,
+           [id]: {
+             ...a,
+             cost: curNode.cost + 1,
+             prev: cur
+           }
+         }
+     }, {...nodes})
 
-      const newShortestPaths = prevShortest
-        .map((r, ty) => r.map((n, tx) => 
-                              (tx === x && ty === y) 
-                                ? prevShortest[cx][cy] + 1 
-                                : n
-                             ))
-
-      const result = dijkstra({grid, shortestPaths: newShortestPaths, cx: x, cy: y})
-      return result
-     } catch (err) { 
-       return prevShortest
+     const newNodes = {
+       ...adjUpdatedNodes,
+       [cur]: {
+         ...adjUpdatedNodes[cur],
+         visited: true
+       }
      }
-  }, shortestPaths)
+
+     const [next] = Object.entries(newNodes)
+       .filter(([,val]) => !val.visited)
+       .sort(([,a],[,b]) => a.cost > b.cost ? 1 : -1)[0]
+
+     return dijkstra({nodes: newNodes, cur: next, end})
+     // return newNodes
 }
 
-const run = (data: string) => {
 
-  const grid = data.split("\n").map(r => r.split("").map(c => {
-    const height = c.charCodeAt(0) - (CHAR_START - 1)
-    if(height < 0) 
-      return height < -14 ? -1 : 0
-    return height
-  }));
+const dijkstraImperative = 
+  ({nodes, start, end}: 
+   {
+     nodes: {[key: string]: Node}, 
+     start: string
+     end?: string
+   }) => {
+     let cur = start
+     nodes[cur].cost = 0
+     while(cur !== end)  {
+         let curNode = nodes[cur]
+         nodes = curNode.adj.reduce((prev, id) => {
+             const a = prev[id]
+             if(a.visited || a.cost < curNode.cost + 1) 
+               return prev;
 
-  printGrid(grid)
+               nodes[id] = {
+                 ...a,
+                 cost: curNode.cost + 1,
+                 prev: cur
+               }
+             return nodes
+         }, nodes)
+
+         curNode.visited = true
+
+         const queue = Object.entries(nodes)
+           .filter(([,val]) => !val.visited)
+           .sort(([,a],[,b]) => a.cost > b.cost ? 1 : -1);
+
+         if(queue.length < 1) return nodes
+         let [next] = queue[0]
+        cur = next
+       }
+      return nodes;
+}
+
+type Node = 
+  {
+    cost: number, 
+    value: number, 
+    prev: string | null, 
+    visited: boolean,
+    adj: string[]
+}
 
 
-  const [sx, sy] = findPos(0, grid);
-  const [ex, ey] = findPos(-1, grid);
+const run = (data: string, part: number) => {
+  const {heights, start, end}: {heights: number[][], start: string, end: string} = 
+    data.split("\n").reduce((prevRow, r, y) => {
+      const {rowHeights, start, end} = 
+        r.split("").reduce(
+          (prev, char, x) => { 
+            const start = char === "S" ? x+","+y : null
+            const end = char === "E" ? x+","+y : null
+            const c = start ? "a" : end ? "z" : char
+            const height = c.charCodeAt(0) - (CHAR_START - 1)
 
-  log("S", sx, sy)
+            return { 
+              rowHeights: [...prev.rowHeights, height], 
+              start: start ? start : prev.start, 
+              end: end ? end : prev.end
+            }
+          }, {rowHeights: [] as number[], start: null as string | null, end: null as string | null})
 
-  const shortestPaths = grid.map((r, y) => r.map((_, x) => x === sx && y === sy ? 0 : Infinity))
+          return {
+            heights: [...prevRow.heights, rowHeights],
+            start: start ? start : prevRow.start, 
+            end: end ? end : prevRow.end
+          }
+  }, {heights: [] as number[][], start: null, end: null})
 
-  const result = dijkstra({grid, shortestPaths, cx: sx, cy: sy})
+  const nodes = heights.reduce((prevGraph1, r,y) => ({
+    ...prevGraph1, 
+    ...r.reduce((prevGraph2, h,x) => ({
+      ...prevGraph2, 
+      [x+","+y]: {
+      cost: Infinity,
+      visited: false,
+      value: h,
+      prev: null,
+      adj: getAdjecent(heights, x, y).filter(([ax, ay]) => !(heights[ay][ax] - heights[y][x] > 1)).map(([ax, ay]) => ax+","+ay)
+    }}), 
+  {} as {[key: string]: Node})}), 
+  {} as {[key: string]: Node})
 
+  const nodes2 = heights.reduce((prevGraph1, r,y) => ({
+    ...prevGraph1, 
+    ...r.reduce((prevGraph2, h,x) => ({
+      ...prevGraph2, 
+      [x+","+y]: {
+      cost: Infinity,
+      visited: false,
+      value: h,
+      prev: null,
+      adj: getAdjecent(heights, x, y).filter(([ax, ay]) => !(heights[y][x] - heights[ay][ax] > 1)).map(([ax, ay]) => ax+","+ay)
+    }}), 
+  {} as {[key: string]: Node})}), 
+  {} as {[key: string]: Node})
+
+  log("Start", start)
+  log("End", end)
+
+  // log(heights.map(r => r.join(",")).join("\n"))
+
+  const result = part === 1 
+    ? dijkstraImperative({nodes, start, end})[end].cost
+    : Math.min(...Object.values(dijkstraImperative({nodes: nodes2, start: end})).filter(n => n.value === 1).map(n => n.cost))
   return result
 }
 
 
-log()
-log("1:") 
-printGrid(run(input))
+log("1:")
+log(run(input, 1))
 log("2:")
-// printGrid(run(input))
+log(run(input, 2))
