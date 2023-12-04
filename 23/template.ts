7ddbd008c502dfe3dce1f747b69e6a205943d042
Lassi Köykkä <me@lassi-koykka.fi>
2023-12-04 14:35:02 +0200

    Update Typescript template

--- 23/template.ts
+++ 23/template.ts
@@ -1,15 +1,31 @@
+import assert from "assert";
 import { getContent } from "./utils";
 
-const TEST = 1;
-const CREATE_FILES = 0;
-const filename = __filename.split(".")[0] + (TEST ? "-test.txt" : "-input.txt");
+const prepareInput = (data: string) => {
+  const lines = data.split("\n").map((l) => l.trim());
+  return lines;
+};
 
-const input = getContent(filename);
-
-const run = (data: string) => {
+const part1 = (data: any) => {
   return data;
 };
 
-console.log();
-console.log("1:", run(input));
-console.log("2:", run(input));
+const part2 = (data: any) => {
+  return data;
+};
+
+const TEST = 1;
+const FILENAME = __filename.split(".")[0] + (TEST ? "-test.txt" : "-input.txt");
+const RESULT_ONE = 0;
+const RESULT_TWO = 0;
+
+const input = getContent(FILENAME);
+const data = prepareInput(input);
+const result1 = part1(data);
+const result2 = part2(data);
+
+TEST && assert(result1 === RESULT_ONE, "RESULT 1 INCORRECT");
+TEST && assert(result2 === RESULT_TWO, "RESULT 2 INCORRECT");
+
+console.log("1:", result1);
+console.log("2:", result2);
