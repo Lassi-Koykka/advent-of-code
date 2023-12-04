6702b145db587387fa862076eb80531479e0d962
Lassi Köykkä <me@lassi-koykka.fi>
2023-12-04 14:14:50 +0200

    Refactor and optimize 2023 day 4 using memoization

--- 23/e04.ts
+++ 23/e04.ts
@@ -5,7 +5,7 @@ const filename = __filename.split(".")[0] + (TEST ? "-test.txt" : "-input.txt");
 
 const input = getContent(filename);
 
-const parseCards = (data: string) =>
+const prepareInput = (data: string) =>
   data.split("\n").map((line) =>
     line
       .replaceAll("  ", " ")
@@ -14,8 +14,7 @@ const parseCards = (data: string) =>
       .map((side) => side.split(" ").map(Number)),
   );
 
-const part1 = (data: string) => {
-  const cards = parseCards(data);
+const part1 = (cards: number[][][]) => {
   const result = cards.reduce((prev, [nums, winNums]) => {
     const matches = nums.filter((num) => winNums.includes(num)).length;
     return matches > 0 ? prev + 2 ** (matches - 1) : prev;
@@ -23,30 +22,39 @@ const part1 = (data: string) => {
   return result;
 };
 
-const part2 = (data: string) => {
-  const cards = parseCards(data);
+const getWonCards = (id: number, winMemo: number[][], cardWins: number[][]) => {
+  if (winMemo[id].length > 0) return winMemo[id];
+  if (cardWins[id].length < 1) return [id];
+  const accCards = cardWins[id].flatMap((wonId) =>
+    getWonCards(wonId, winMemo, cardWins),
+  );
+  return [id, ...accCards];
+};
 
+const part2 = (cards: number[][][]) => {
   const cardWins = cards.reduce((prevWins, [nums, winNums], idx) => {
     const matches = nums.filter((num) => winNums.includes(num)).length;
-    const wonCopies = cards
-      .map((_, id) => id)
-      .slice(idx + 1, idx + matches + 1);
+    const wonCopies = cards.map((_, i) => i).slice(idx + 1, idx + matches + 1);
     return [...prevWins, wonCopies];
   }, [] as number[][]);
 
-  const getAllCards = (idx: number): number[] => {
-    if (cardWins[idx].length < 1) return [idx];
-    return [idx, ...cardWins[idx].flatMap((wonId) => getAllCards(wonId))];
-  };
+  const cardIds = cards.map((_, id) => id).reverse();
 
-  const result = cards.reduce(
-    (prevSum, _, idx) => prevSum + getAllCards(idx).length,
-    0,
+  const { sum } = cardIds.reduce(
+    (prev, id) => {
+      const { sum: prevSum, cardMemo } = prev;
+      const cardWonCards = getWonCards(id, cardMemo, cardWins);
+      const newCardMemo = cardMemo.map((t, memoIdx) =>
+        memoIdx !== id ? t : cardWonCards,
+      );
+      return { sum: prevSum + cardWonCards.length, cardMemo: newCardMemo };
+    },
+    { sum: 0, cardMemo: new Array(cards.length).fill([] as number[]) },
   );
 
-  return result;
+  return sum;
 };
 
-console.log();
-console.log("1:", part1(input));
-console.log("2:", part2(input));
+const cards = prepareInput(input);
+console.log("1:", part1(cards));
+console.log("2:", part2(cards));
