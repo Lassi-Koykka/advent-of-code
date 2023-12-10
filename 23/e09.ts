d09a75d45ef37f018b1966c231cb00f14d44e489
Lassi Köykkä <lassi_koykka@hotmail.com>
2023-12-10 14:15:37 +0200

    Finish 2023 day 9

--- 23/e09.ts
+++ 23/e09.ts
@@ -1,20 +1,47 @@
 import assert from "assert";
 
 import { getContent } from "./utils";
-const TEST = 1;
+const TEST = 0;
 const FILENAME = __filename.split(".")[0] + (TEST ? "-test.txt" : "-input.txt");
-const RESULT_ONE = 0;
-const RESULT_TWO = 0;
+const RESULT_ONE = 114;
+const RESULT_TWO = 2;
 
 const prepareInput = (data: string) =>
   data.split("\n").map((l) => l.split(" ").map(Number));
 
-const part1 = (data: any) => {
-  return data;
+const getDiffs = (arr: number[]) =>
+  arr.reduce((prev, x, i) => {
+    return i !== 0 ? [...prev, x - arr[i - 1]] : prev;
+  }, [] as number[]);
+
+const getHistories = (histories: number[][]): number[][] => {
+  const cur = histories.at(-1);
+  if (cur.every((x) => x === 0)) return histories;
+  return getHistories([...histories, getDiffs(cur)]);
 };
 
-const part2 = (data: any) => {
-  return data;
+const part1 = (data: number[][]) => {
+  return data.reduce((sum, d) => {
+    const histories = getHistories([d]);
+    const extrapolation = histories.reduceRight(
+      (prev, h, i) =>
+        i !== histories.length - 1 ? [...prev, h.at(-1) + prev.at(-1)] : prev,
+      [0] as number[],
+    );
+    return sum + extrapolation.at(-1);
+  }, 0);
+};
+
+const part2 = (data: number[][]) => {
+  return data.reduce((sum, d) => {
+    const histories = getHistories([d]).map((h) => h.reverse());
+    const extrapolation = histories.reduceRight(
+      (prev, h, i) =>
+        i !== histories.length - 1 ? [...prev, h.at(-1) - prev.at(-1)] : prev,
+      [0] as number[],
+    );
+    return sum + extrapolation.at(-1);
+  }, 0);
 };
 
 const input = getContent(FILENAME);
